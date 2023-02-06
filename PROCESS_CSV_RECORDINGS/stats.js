03508590806338d81a159b777cf33d9d628fb686
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-02-06 13:33:11 +0100

    updated exports

--- PROCESS_CSV_RECORDINGS/stats.js
+++ PROCESS_CSV_RECORDINGS/stats.js
@@ -1,12 +1,9 @@
-const sum = arr => arr.reduce((total, el) => total + el, 0);
-const mean = arr => sum(arr) / arr.length;
-const median = arr => [...[...arr].sort()][Math.floor(arr.length / 2)];
-
-const calculateStats = arr => ({
+export const sum = arr => arr.reduce((total, el) => total + el, 0);
+export const mean = arr => sum(arr) / arr.length;
+export const median = arr => [...[...arr].sort()][Math.floor(arr.length / 2)];
+export const sumaryStats = arr => ({
     mean: mean(arr),
     median: median(arr),
     min: Math.min(...arr),
     max: Math.max(...arr)
 });
-
-module.exports = { calculateStats, mean, median };
