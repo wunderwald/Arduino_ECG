8761fa9567acf24b5e48c977ff7475575dd57fb6
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-02-06 17:05:16 +0100

    created structure

---
+++ PEAK_DETECTION/detectPeaks.py
@@ -0,0 +1,16 @@
+import neurokit2 as nk
+
+# get list of input files
+
+# read file
+
+# detect peaks
+
+# write to json file that is compatible with peak correction dashboard:
+# form:
+# ecg: [{index, ecg}...]
+# peaks [peakIndices...]
+# "startIndex": 0,
+# "endIndex": 180094,
+# "removedRegions": [],
+# "samplingRate": 1000
