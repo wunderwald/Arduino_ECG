80b6bd2fccaedd8b242306e34808ad14e46249ef
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-02-22 12:35:46 +0100

    adapted min / max for large arrays

--- UNITY_RTT/PROCESSING/stats.js
+++ UNITY_RTT/PROCESSING/stats.js
@@ -4,13 +4,14 @@ https://github.com/wunderwald/
 moritzwunderwald@yahoo.de
 */
 
-
+export const min = arr => arr.reduce((currentMin, next) => next < currentMin ? next : currentMin, Number.POSITIVE_INFINITY);
+export const max = arr => arr.reduce((currentMax, next) => next > currentMax ? next : currentMax, Number.NEGATIVE_INFINITY);
 export const sum = arr => arr.reduce((total, el) => total + el, 0);
 export const mean = arr => sum(arr) / arr.length;
 export const median = arr => [...[...arr].sort()][Math.floor(arr.length / 2)];
 export const sumaryStats = arr => ({
     mean: mean(arr),
     median: median(arr),
-    min: Math.min(...arr),
-    max: Math.max(...arr)
+    min: min(arr),
+    max: max(arr)
 });
