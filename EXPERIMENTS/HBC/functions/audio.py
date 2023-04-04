3633714418df1db8c675f2441ecb16319c5d73d6
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-04-04 17:19:04 +0200

    added audio through ffplay

---
+++ EXPERIMENTS/HBC/functions/audio.py
@@ -0,0 +1,9 @@
+import subprocess
+
+audio_file = './media/bleep.wav'
+
+# command to run ffplay with audio file as input
+playAudioFFPlay = ['ffplay', '-nodisp', '-autoexit', '-loglevel', 'panic', audio_file]
+
+def playSound():
+    subprocess.call(playAudioFFPlay)
